2dfbff7b8f58ac6a54e9c1249471cef9da601c23
ebhazarcodes <erdoganbaranhazar@gmail.com>
2023-08-29 16:36:31 +0300

    identified account api mapping

--- account-service/src/main/java/com/springcloudmicroservice/accountservice/api/AccountApi.java
+++ account-service/src/main/java/com/springcloudmicroservice/accountservice/api/AccountApi.java
@@ -9,22 +9,30 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("account")
 public class AccountApi {
 
-    public ResponseEntity<Account> get(String id){
+    private final AccountService accountService;
 
+    public AccountApi(AccountService accountService) {
+        this.accountService = accountService;
     }
-    public ResponseEntity<Account> save(Account account){
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Account> get(@PathVariable("id") String id){
+        return ResponseEntity.ok(accountService.get(id));
     }
+
+    @PostMapping
+    public ResponseEntity<Account> save(@RequestBody Account account){
+        return ResponseEntity.ok(accountService.save(account));
+    }
+
+    @PutMapping
     public ResponseEntity<Account> update(Account account){
-
+        return ResponseEntity.ok(accountService.update(account));
     }
+
+    @DeleteMapping
     public void delete(String id){
-
+        accountService.delete(id);
     }
-    public ResponseEntity<Account> pagination(){
-
-    }
-
-
 
 }
