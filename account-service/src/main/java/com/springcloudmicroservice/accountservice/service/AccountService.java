b956450f31ef72656a537169078f151d8f6ea53a
ebhazarcodes <erdoganbaranhazar@gmail.com>
2023-08-29 16:37:23 +0300

    updated account service method

--- account-service/src/main/java/com/springcloudmicroservice/accountservice/service/AccountService.java
+++ account-service/src/main/java/com/springcloudmicroservice/accountservice/service/AccountService.java
@@ -8,18 +8,16 @@ import org.springframework.stereotype.Service;
 public class AccountService {
 
     public Account get(String id){
-
+        return new Account("test-id");
     }
     public Account save(Account account){
-
+        return new Account("test-id");
     }
     public Account update(Account account){
-
+        return new Account("test-id");
     }
     public void delete(String id){
 
     }
-    public Account pagination(){
 
-    }
 }
