40752efd83cb3a823ff2ea926652c6bb1f4a0ac3
ebhazarcodes <erdoganbaranhazar@gmail.com>
2023-08-29 15:48:23 +0300

    create account api

---
+++ account-service/src/main/java/com/springcloudmicroservice/accountservice/api/AccountApi.java
@@ -0,0 +1,26 @@
+package com.springcloudmicroservice.accountservice.api;
+
+import com.springcloudmicroservice.accountservice.entity.Account;
+import org.springframework.http.ResponseEntity;
+
+public class AccountApi {
+
+    public ResponseEntity<Account> get(){
+
+    }
+    public ResponseEntity<Account> save(){
+
+    }
+    public ResponseEntity<Account> update(){
+
+    }
+    public ResponseEntity<Account> delete(){
+
+    }
+    public ResponseEntity<Account> pagination(){
+
+    }
+
+
+
+}
