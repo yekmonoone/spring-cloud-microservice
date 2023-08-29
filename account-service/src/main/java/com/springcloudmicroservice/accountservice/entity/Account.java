6890c624964f91981bd4ffeb1a98fe9a47d8bac5
ebhazarcodes <erdoganbaranhazar@gmail.com>
2023-08-29 15:41:06 +0300

    create account entity

---
+++ account-service/src/main/java/com/springcloudmicroservice/accountservice/entity/Account.java
@@ -0,0 +1,40 @@
+package com.springcloudmicroservice.accountservice.entity;
+
+public class Account {
+    private String id;
+    private String username;
+    private String email;
+    private String passwd;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+}
