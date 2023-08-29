d3f7a72c56ba4cbe117c40007a8ab4377e4413fc
ebhazarcodes <erdoganbaranhazar@gmail.com>
2023-08-29 15:59:55 +0300

    create account service class

---
+++ account-service/src/main/java/com/springcloudmicroservice/accountservice/service/AccountService.java
@@ -0,0 +1,23 @@
+package com.springcloudmicroservice.accountservice.service;
+
+import com.springcloudmicroservice.accountservice.entity.Account;
+import org.springframework.http.ResponseEntity;
+
+public class AccountService {
+
+    public Account get(String id){
+
+    }
+    public Account save(Account account){
+
+    }
+    public Account update(Account account){
+
+    }
+    public void delete(String id){
+
+    }
+    public Account pagination(){
+
+    }
+}
