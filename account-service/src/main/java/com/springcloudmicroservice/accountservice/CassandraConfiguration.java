bb3857cd8b5ec9afbf94c81e402f904b36003d13
ebhazarcodes <erdoganbaranhazar@gmail.com>
2023-09-06 14:38:58 +0300

    added schema action to Cassandra

--- account-service/src/main/java/com/springcloudmicroservice/accountservice/CassandraConfiguration.java
+++ account-service/src/main/java/com/springcloudmicroservice/accountservice/CassandraConfiguration.java
@@ -3,6 +3,7 @@ package com.springcloudmicroservice.accountservice;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
@@ -32,5 +33,11 @@ public class CassandraConfiguration extends AbstractCassandraConfiguration {
     @Override
     protected String getContactPoints() {
         return contactPoint;
+    }
+
+    // if already table exists then it will not create table again
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
     }
 }
